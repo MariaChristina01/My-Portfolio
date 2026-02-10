39cbe195da8dee3928ce420f2c21d7c8258d94a9
Christina Castillejo <1-230312@asiatech.edu.ph>
2026-02-10 22:30:28 +0800

    Hover animation update

--- scroll.js
+++ scroll.js
@@ -1,19 +1,39 @@
-const sections = document.querySelectorAll("section");
-const navLi = document.querySelectorAll("nav ul li a");
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
 
-document.addEventListener("scroll", () => {
-  let current = "";
-    sections.forEach((section) => {
-        const sectionTop = section.offsetTop - 80;
-        const sectionHeight = section.clientHeight;
-        if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
-            current = section.getAttribute("id");
-        }
-    });
-    navLi.forEach((li) => {
-        li.classList.remove("active");
-        if (li.getAttribute("href").includes(current)) {
-            li.classList.add("active");
-        }
-    });
-});
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
